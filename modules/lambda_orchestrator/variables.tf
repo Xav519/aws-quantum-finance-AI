a37dac92fb467da59c4011e2d66180b283fd63f7
Xav519 <dupuis_x@yahoo.com>
2026-03-25 20:55:02 -0400

    First variable push - base

---
+++ modules/lambda_orchestrator/variables.tf
@@ -0,0 +1,15 @@
+
+/*
+variable "project"               { type = string }
+variable "environment"           { type = string }
+variable "lambda_role_arn"       { type = string }
+variable "lambda_classical_name" { type = string }
+variable "lambda_braket_name"    { type = string }
+variable "dynamodb_table_name"   { type = string }
+
+variable "classical_threshold" {
+  description = "N <= this value uses classical brute-force. N > this uses quantum QAOA."
+  type        = number
+  default     = 4
+}
+*/
