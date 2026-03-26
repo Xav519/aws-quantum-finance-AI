8898f8e8ca01fbd6bbe6f05f2f06d61937abcc3f
Xav519 <dupuis_x@yahoo.com>
2026-03-25 20:55:26 -0400

    temp push

---
+++ modules/lambda_orchestrator/main.tf
@@ -0,0 +1,12 @@
+
+/*
+environment {
+  variables = {
+    LAMBDA_CLASSICAL    = var.lambda_classical_name
+    LAMBDA_BRAKET       = var.lambda_braket_name
+    CLASSICAL_THRESHOLD = tostring(var.classical_threshold)
+    DYNAMODB_TABLE      = var.dynamodb_table_name
+    BEDROCK_MODEL       = "anthropic.claude-haiku-4-5-20251001"
+  }
+}
+*/
