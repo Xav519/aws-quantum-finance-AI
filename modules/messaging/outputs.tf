e3d1bd83dedd1c5bac594d83455699a7265b0c58
Xav519 <dupuis_x@yahoo.com>
2026-03-26 21:41:30 -0400

    modified output

--- modules/messaging/outputs.tf
+++ modules/messaging/outputs.tf
@@ -1,9 +1,9 @@
 output "eventbridge_rule_arn" {
-  description = "ARN of the EventBridge rule for Braket job completion"
-  value       = aws_cloudwatch_event_rule.braket_job_complete.arn
+  description = "ARN of the EventBridge rule that triggers Lambda Braket on task completion"
+  value       = aws_cloudwatch_event_rule.braket_task_complete.arn
 }
 
 output "eventbridge_rule_name" {
   description = "Name of the EventBridge rule"
-  value       = aws_cloudwatch_event_rule.braket_job_complete.name
+  value       = aws_cloudwatch_event_rule.braket_task_complete.name
 }
