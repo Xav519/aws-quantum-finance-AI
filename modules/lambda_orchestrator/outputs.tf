1676af4d6490f6bc1ac6fd06b548bfefcfbca0da
Xav519 <dupuis_x@yahoo.com>
2026-03-26 21:48:40 -0400

    corrected output naming

--- modules/lambda_orchestrator/outputs.tf
+++ modules/lambda_orchestrator/outputs.tf
@@ -5,5 +5,5 @@ output "function_arn" {
 
 output "function_name" {
   description = "Lambda function name"
-  value       = aws_lambda_function.orchestrator.name
+  value       = aws_lambda_function.orchestrator.function_name
 }
