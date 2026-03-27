0fc3de22755eb01ce647ecd9aa06778b30be3def
Xav519 <dupuis_x@yahoo.com>
2026-03-26 21:40:03 -0400

    modified output

--- modules/lambda_orchestrator/outputs.tf
+++ modules/lambda_orchestrator/outputs.tf
@@ -1,7 +1,9 @@
 output "function_arn" {
-  value = aws_lambda_function.orchestrator.arn
+  description = "Lambda function ARN"
+  value       = aws_lambda_function.orchestrator.arn
 }
 
 output "function_name" {
-  value = aws_lambda_function.orchestrator.function_name
+  description = "Lambda function name"
+  value       = aws_lambda_function.orchestrator.name
 }
