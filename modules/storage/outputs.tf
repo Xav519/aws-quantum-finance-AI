e7970e64487bdea4adde02f29c425e57d7b42eec
Xav519 <dupuis_x@yahoo.com>
2026-03-26 21:33:41 -0400

    Added dynamodb_table_name ref in outputs

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -3,6 +3,11 @@ output "results_bucket_name" {
   value       = aws_s3_bucket.braket_results.bucket
 }
 
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB jobs table"
+  value       = aws_dynamodb_table.jobs.name
+}
+
 output "results_bucket_arn" {
   description = "ARN of the S3 bucket for Braket results"
   value       = aws_s3_bucket.braket_results.arn
